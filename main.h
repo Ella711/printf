087e2258af2f8cf76e31875fa6687ee1604ffb58
MaoScorpio21 <3917@holbertonschool.com>
2021-11-06 18:00:12 -0500

    main.h - main.c

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <stlib.h>
+#include <stdarg.h>
+
+/**
+ * main.h -- Nuestra librería local para el proyecto 0x11.C-printf
+ * Authors: Ornella Russo - Marco Antonio Ordóñez
+ */
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
