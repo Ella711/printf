4cb6d0ce117b2e0e0767e6de40fa6975b76c3608
Ella711 <allenro711@gmail.com>
2021-11-08 14:33:42 -0500

    [ADD] header file

--- main.h
+++ main.h
@@ -1,17 +1,31 @@
 #ifndef MAIN_H
 #define MAIN_H
+/* LIBRARIES */
 
-#include <limits.h>
-#include <stdio.h>
-#include <stlib.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
 /**
- * main.h -- Nuestra librería local para el proyecto 0x11.C-printf
+ * main.h -- Main header file for 0x11.C-printf
  * Authors: Ornella Russo - Marco Antonio Ordóñez
  */
 
+/* secondary functions */
+
+/* picks a functions based on format specifier */
+void* (*get_spec_func(char c))(va_list);
+
+/* printf functions */
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_char(va_list);
+/* char *print_int(va_list); can be used for char and decimal (%d %i)? */
+int print_int(va_list);
+/* char *print_special(va_list); %% */
+int print_string(va_list);
 
-#endif
+
+#endif /* MAIN_H */
+
