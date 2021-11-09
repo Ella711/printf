87842ebc8c95811e4736f76dcda5f44dbca3e580
Ella711 <allenro711@gmail.com>
2021-11-08 20:59:10 -0500

    [ADD] auxiliary functions needed

---
+++ aux_funcs.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _strlen - returns length of string
+*@s: a string
+*Return: i
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
+/**
+* _strcpy - prints n elements of array
+*@dest: string
+*@src: string
+*Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = src[i];
+	return (dest);
+}
