964155c1883fef7a9ae4abc08e6066c14aeb388f
Ella711 <allenro711@gmail.com>
2021-11-08 21:21:31 -0500

    [FIX] Betty styling

--- spec_functions.c
+++ spec_functions.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * print_char - prints a char
+ *@arguements: valist
+ * Return: a char in a string
+ */
 char *print_char(va_list arguements)
 {
 	char *str;
@@ -13,13 +17,17 @@ char *print_char(va_list arguements)
 	str = malloc(sizeof(char) * 2);
 	if (str == NULL)
 		return (NULL);
-	
+
 	str[0] = c;
 	str[1] = '\0';
 
 	return (str);
 }
-
+/**
+ * print_string - prints a string
+ *@arguements: valist
+ * Return: a string
+ */
 char *print_string(va_list arguements)
 {
 	char *str;
@@ -29,12 +37,13 @@ char *print_string(va_list arguements)
 	str = va_arg(arguements, char *);
 	if (str == NULL)
 		str = "(null)";
-	
+
 	len = _strlen(str);
 
 	copy = malloc(sizeof(char) * len + 1);
 	if (copy == NULL)
 		return (NULL);
-	
+
 	return (_strcpy(copy, str));
 }
+
