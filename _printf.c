25974b97d926e07c9db0d59e58b46c0f4bc7d1ef
Ella711 <allenro711@gmail.com>
2021-11-08 20:58:51 -0500

    [UPDATE] printf ith structs

--- _printf.c
+++ _printf.c
@@ -1,43 +1,83 @@
 #include "main.h"
-/**
- * _printf - prints
- * @format: list of what needs to be printed
- * Return: int
- */
+
 int _printf(const char *format, ...)
 {
-	const char *format_pointer = format;
-	va_list list;
+    int length = 0, tot_length = 0, i = 0, j = 0;
+    char *buffer, *str;
+    char* (*func)(va_list);
+    va_list arguements;
 
-	va_start(list, format);
+    if (format == NULL)
+        return (-1);
+    
+    buffer = create_buffer();
+    if (buffer == NULL)
+        return (-1);
+    va_start(arguements, format);
 
-	while (*format_pointer && *format)
-	{
-		if (*format_pointer != '%')
-		{
-			_putchar(*format_pointer);
-			format_pointer++;
-			continue;
-		}
-		format_pointer++;
-		switch (*format_pointer)
-		{
-			case 'c':
-				print_char(list);
-				break;
-			case 's':
-				print_string(list);
-				break;
-			case 'd':
-				print_int(list);
-				break;
-			default:
-				format_pointer++;
-				continue;
-		}
-		format_pointer++;
-	}
-	va_end(list);
-	return (0);
+    while(format[i] != '\0')
+    {
+        if(format[i] != '%') /* copy format into buffer till % */
+        {
+            length = check_buffer_length(buffer, length);
+            buffer[length++] = format[i++];
+            tot_length++;
+        }
+        else /* if format[i] = % */ 
+        {
+            i++;
+            if (format[i] == '\0') /* nothing after the % */
+            {
+                va_end(arguements);
+                free(buffer);
+                return(-1);
+            }
+            if (format[i] == '%') /* handles %% */
+            {
+                length = check_buffer_length(buffer, length);
+                buffer[length++] = format[i];
+                tot_length++;
+            }
+            else
+            {
+                func = get_spec_func(format[i]); /* looks up required function */
+                if (func == NULL) /* if it doesn't find a spec, print % */
+                {
+                    length = check_buffer_length(buffer, length);
+                    buffer[length++] = '%';
+                    tot_length++;
+                    buffer[length++] = format[i];
+                    tot_length++;
+                }
+                else /* func will return str and copy to buffer */
+                {
+                    str = func(arguements);
+                    if (str == NULL)
+                    {
+                        va_end(arguements);
+                        free(buffer);
+                        return (-1);
+                    }
+                    if (format[i] == 'c' && str[0] == '\0')
+                    {
+                        length = check_buffer_length(buffer, length);
+                        buffer[length++] = '\0';
+                        tot_length++;
+                    }
+                    j = 0;
+                    while (str[j] != '\0')
+                    {
+                        length = check_buffer_length(buffer, length);
+                        buffer[length++] = str[j];
+                        tot_length++;
+                        j++;
+                    }
+                    free(str);
+                }
+            }
+            i++;
+        }
+    }
+    write_buffer(buffer, length, arguements);
+    return (tot_length);
 }
-
